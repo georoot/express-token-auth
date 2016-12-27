cdb3177fcae5407b422938cbed8e40c85bc5a23c
georoot <rb1223334444@gmail.com>
2016-12-27 15:49:43 +0530

    Addition of email to models

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
+require('mongoose-type-email');
 var Schema = mongoose.Schema;
 
 // set up a mongoose model and pass it using module.exports
 module.exports = mongoose.model('User', new Schema({
-    email: String,
-    password: String,
+    email: {type: mongoose.SchemaTypes.Email, required: true,index: { unique: true }},
+    password: {type: String, required: true},
     verified: Boolean,
     admin: Boolean
 }));
